9a8708e8c67122f437285f4f70fb3f298eb3d7f4
mariegautron <marigaut2@gmail.com>
2022-01-06 10:41:12 +0100

    list of request

---
+++ src/components/Container.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Container: FC = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
+
+export default Container;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,30 @@
-const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+import RequestList from "../components/RequestList";
 
-export default Home
+const Home = () => {
+  const requests = [
+    {
+      profilPicture: "https://picsum.photos/200/300",
+      name: "Ma requète trop cool",
+      questGiver: "Tartin",
+      bounty: 20.3,
+      duration: 5,
+      startDate: new Date(),
+    },
+    {
+      profilPicture: "https://picsum.photos/200/300",
+      name: "Ma requète trop cool 2",
+      questGiver: "Tartin",
+      bounty: 20.3,
+      duration: 5,
+      startDate: new Date(),
+    },
+  ];
+
+  return (
+    <div>
+      <RequestList requests={requests} />
+    </div>
+  );
+};
+
+export default Home;
