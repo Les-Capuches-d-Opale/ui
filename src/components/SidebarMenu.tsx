99911f17e1ccbb66c4cec61cbb428946a170c55a
Chloengr <53560772+Chloengr@users.noreply.github.com>
2022-01-06 10:27:08 +0100

    sidebar with style

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,8 +1,6 @@
 import { NavLink } from "react-router-dom";
 import Routes, { MenuItem } from "../sdk/routes";
 
-
-
 const menuItem: MenuItem[] = [
   {
     label: "Accueil",
@@ -36,7 +34,19 @@ const SidebarMenu = () => {
       <nav className="sidebar">
         {menuItem.map((item, index) => {
           return (
-            <NavLink key={index} to={item.to} role="navigation-item">
+            <NavLink
+              key={index}
+              to={item.to}
+              role="navigation-item"
+              activeStyle={{
+                color: "#eeeeee",
+                textDecoration: "none",
+                backgroundColor: "#00cda5",
+                borderRadius: "0 30px 30px 0",
+                padding: "20px",
+                boxShadow: "rgb(0 205 165 / 24%) 0 8px 16px 0",
+              }}
+            >
               {item.label}
             </NavLink>
           );
