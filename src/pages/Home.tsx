ce15181f5d309423b6321b8aede1935879b73826
mariegautron <marigaut2@gmail.com>
2022-01-13 16:32:03 +0100

    test import not used

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,20 @@
-import { useQuery } from "react-query";
-import { Spinner } from "react-rainbow-components";
-import request from "../axios";
-import RequestList from "../components/Home/RequestList/RequestList";
+import { useQuery } from 'react-query';
+import { Spinner } from 'react-rainbow-components';
+import request from '../axios';
+import RequestList from '../components/Home/RequestList/RequestList';
+import Item from './Item';
 
 const Home = () => {
-  const { isLoading, data: dataRequest } = useQuery("fetchRequest", () =>
-    request.get("/requests")
-  );
+    const { isLoading, data: dataRequest } = useQuery('fetchRequest', () =>
+        request.get('/requests'),
+    );
 
-  return (
-    <div>
-      {isLoading && <Spinner />}
-      <RequestList requests={dataRequest?.data} />
-    </div>
-  );
+    return (
+        <div>
+            {isLoading && <Spinner />}
+            <RequestList requests={dataRequest?.data} />
+        </div>
+    );
 };
 
 export default Home;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,14 @@
-import LoginForm from "../components/LoginForm";
+import LoginForm from '../components/LoginForm';
 
 const Login = () => {
-  return (
-    <div className="formLayout">
-      <h1 style={{ width: "400px" }}>
-        Connectez-vous pour accèder à votre espace administrateur
-      </h1>
-      <LoginForm />
-    </div>
-  );
+    return (
+        <div className="formLayout">
+            <h1 style={{ width: '400px' }}>
+                Connectez-vous pour accèder à votre espace administrateur
+            </h1>
+            <LoginForm />
+        </div>
+    );
 };
 
 export default Login;
