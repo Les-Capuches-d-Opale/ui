2c162ee6ac38f0a3ee98cf5568eb29ac660541e5
mariegautron <marigaut2@gmail.com>
2022-01-13 16:34:57 +0100

    test import not used

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,45 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 13,
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
-    "react/no-unescaped-entities": "off",
-    "@typescript-eslint/ban-types": [
-      "error",
-      {
-        types: {
-          String: false,
-          Boolean: false,
-          Number: false,
-          Symbol: false,
-          "{}": false,
-          Object: false,
-          object: false,
-          Function: false,
-        },
-        extendDefaults: true,
-      },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
     ],
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 13,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/no-unused-vars': 'on',
+        'unused-imports/no-unused-imports-ts': 'on',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+        'react/no-unescaped-entities': 'off',
+        '@typescript-eslint/ban-types': [
+            'error',
+            {
+                types: {
+                    String: false,
+                    Boolean: false,
+                    Number: false,
+                    Symbol: false,
+                    '{}': false,
+                    Object: false,
+                    object: false,
+                    Function: false,
+                },
+                extendDefaults: true,
+            },
+        ],
+    },
 };
