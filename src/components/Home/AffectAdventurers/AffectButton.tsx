9c3c898c8726aa9c25e2474bd729be89e24f3c34
mariegautron <marigaut2@gmail.com>
2022-01-19 23:46:53 +0100

    link to route

--- src/components/Home/AffectAdventurers/AffectButton.tsx
+++ src/components/Home/AffectAdventurers/AffectButton.tsx
@@ -6,8 +6,16 @@ import axiosRequest from "../../../axios";
 import { FilteredRequiredAdventurer } from "../../../contexts/adventurersAffected";
 import Routes from "../../../sdk/routes";
 
-interface RequestFormType {
-  groups: FilteredRequiredAdventurer[];
+interface Group {
+  reqProfile: {
+    id: string | undefined;
+    experience: number | undefined;
+  };
+  adventurer: string;
+}
+
+interface PayloadAffectAdventurer {
+  groups: Group[];
   request: string;
 }
 
@@ -19,15 +27,27 @@ interface ButtonProps {
 
 const AffectButton = ({ groups, request, disabled }: ButtonProps) => {
   const history = useHistory();
-  const { mutateAsync } = useMutation<AxiosResponse, Error, RequestFormType>(
-    (params) => axiosRequest.post("quests", params)
-  );
+  const { mutateAsync } = useMutation<
+    AxiosResponse,
+    Error,
+    PayloadAffectAdventurer
+  >((params) => axiosRequest.post("quests", params));
+
+  const formatGroups: Group[] = groups.map(({ reqProfile, adventurer }) => {
+    return {
+      reqProfile: {
+        id: reqProfile?.speciality._id,
+        experience: reqProfile?.experience,
+      },
+      adventurer: adventurer._id,
+    };
+  });
 
   return (
     <Button
       variant="brand"
       onClick={async () => {
-        await mutateAsync({ groups, request });
+        await mutateAsync({ groups: formatGroups, request });
         history.push(Routes.QUESTS);
       }}
       disabled={disabled}
