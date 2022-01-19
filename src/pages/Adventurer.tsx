e52149e4fc7b68d94b68ec9b4b5aecb662c690a4
killiancambert <killian.cambert@ynov.com>
2022-01-19 23:55:47 +0100

    Revert "feat() : add adventurer form"
    This reverts commit fca32707a8a4a03e9ca669cccef04ded410ab1cb.

--- src/pages/Adventurer.tsx
+++ src/pages/Adventurer.tsx
@@ -1,10 +1,6 @@
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
 import { useQuery } from "react-query";
-import { ButtonIcon, Spinner } from "react-rainbow-components";
+import { Spinner } from "react-rainbow-components";
 import request from "../axios";
-import ModalAdventurerForm from "../components/AdventurerForm";
 import AdventurersList from "../components/AdventurersList";
 import Container from "../components/Core/Container";
 
@@ -14,37 +10,13 @@ const Adventurer = () => {
     () => request.get("/adventurers")
   );
 
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOnClick = () => {
-    setIsOpen(true);
-  };
-
   return (
     <Container>
       {isLoading ? (
         <Spinner />
       ) : (
         <>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <h1>Liste des aventuriers</h1>
-            <span>
-              <ButtonIcon
-                variant="neutral"
-                tooltip="Créer une requête"
-                icon={<FontAwesomeIcon icon={faPlus} />}
-                onClick={() => handleOnClick()}
-              />
-              <ModalAdventurerForm isOpen={isOpen} setOpen={setIsOpen} />
-            </span>
-          </div>
+          <h1>Liste des aventuriers</h1>
           <AdventurersList adventurers={dataAdventurers?.data} />
         </>
       )}
