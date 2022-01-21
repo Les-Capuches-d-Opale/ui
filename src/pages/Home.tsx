aea1e686de57de6d4d55d2adc794fe2d7eec78f0
mariegautron <marigaut2@gmail.com>
2022-01-21 08:06:37 +0100

    filter bounty & name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,11 @@
-import { useState } from "react";
 import { useQuery } from "react-query";
-import {
-  Spinner,
-  Input,
-  DateTimePicker,
-  Picklist,
-  Option,
-} from "react-rainbow-components";
+import { Spinner } from "react-rainbow-components";
 import request from "../axios";
 import RequestList from "../components/Home/RequestList/RequestList";
 
-const containerStyles = {
-  display: "flex",
-  justifyContent: "center",
-  padding: "30px 0",
-};
-const inputStyles = {
-  width: "150px",
-  marginRight: "10px",
-};
-
 const Home = () => {
-  const [bountyMin, setBountyMin] = useState<string>();
-  const [bountyMax, setBountyMax] = useState<string>();
-  const [xp, setXp] = useState<string>();
-  const [questGiver, setQuestGiver] = useState<string>();
-  const [name, setName] = useState<string>();
-  const [duration, setDuration] = useState<string>();
-
   const { isLoading, data: dataRequest } = useQuery("fetchRequest", () =>
-    request.get("/requests", {
-      params: {
-        bountyMin: bountyMin,
-        bountyMax: bountyMax,
-        awardedExperience: xp,
-        questGiver: questGiver,
-        name: name,
-        duration: duration,
-      },
-    })
+    request.get("/requests")
   );
 
   return (
