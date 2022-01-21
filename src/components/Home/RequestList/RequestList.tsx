63539de92defc50e16a8f7c9d06e494cd0129062
mariegautron <marigaut2@gmail.com>
2022-01-21 08:06:37 +0100

    filter bounty & name

--- src/components/Home/RequestList/RequestList.tsx
+++ src/components/Home/RequestList/RequestList.tsx
@@ -1,16 +1,25 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC, useState } from "react";
+import { useQuery } from "react-query";
 import {
   Accordion,
   AccordionSection,
   ButtonIcon,
+  Spinner,
 } from "react-rainbow-components";
-import { RequestListType } from "../../../sdk/request";
+import request from "../../../axios";
+import { Request, RequestListType } from "../../../sdk/request";
 import Container from "../../Core/Container";
 import ModalRequestForm from "../../RequestForm";
 import BadgeList from "./BadgeList";
+import Filter from "./Filter";
 import Label from "./LabelAccordionRequest";
+
+export interface QueryParams {
+  name?: string;
+  bountyMin?: number;
+}
 
 const RequestList: FC<RequestListType> = ({ requests }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,6 +27,23 @@ const RequestList: FC<RequestListType> = ({ requests }) => {
   const handleOnClick = () => {
     setIsOpen(true);
   };
+
+  const [filters, setFilters] = useState<QueryParams>({
+    bountyMin: 1,
+    name: undefined,
+  });
+
+  const { isLoading, data: dataRequest } = useQuery<Request[]>(
+    ["request", filters],
+    ({ queryKey }) =>
+      request.get("/requests", {
+        params: {
+          bountyMin: (queryKey[1] as QueryParams).bountyMin,
+          name: (queryKey[1] as QueryParams).name,
+        },
+      })
+  );
+  const requestData = (dataRequest as any)?.data || requests;
 
   return (
     <Container>
@@ -40,23 +66,31 @@ const RequestList: FC<RequestListType> = ({ requests }) => {
           <ModalRequestForm isOpen={isOpen} setOpen={setIsOpen} />
         </span>
       </div>
-      {!requests ||
-        (requests.length === 0 && <p> Pas de requètes à affecter</p>)}
-      {requests && requests.length > 0 && (
-        <Accordion>
-          {requests.map((req) => {
-            return (
-              <AccordionSection label={<Label {...req} />} key={req._id}>
-                {req.description}
-                <div style={{ opacity: 0.5, marginTop: 5 }}>
-                  Profils requis :
-                </div>
-                <BadgeList requiredProfiles={req.requiredProfiles} />
-              </AccordionSection>
-            );
-          })}
-        </Accordion>
-      )}
+
+      <>
+        {!requestData ||
+          (requestData.length === 0 && <p> Pas de requètes correspondantes</p>)}
+        <>
+          <Filter setFiltered={setFilters} filtered={filters} />
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <Accordion>
+              {requestData.map((req: any) => {
+                return (
+                  <AccordionSection label={<Label {...req} />} key={req._id}>
+                    {req.description}
+                    <div style={{ opacity: 0.5, marginTop: 5 }}>
+                      Profils requis :
+                    </div>
+                    <BadgeList requiredProfiles={req.requiredProfiles} />
+                  </AccordionSection>
+                );
+              })}
+            </Accordion>
+          )}
+        </>
+      </>
     </Container>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,88 +1,16 @@
-import { useState } from 'react';
 import { useQuery } from "react-query";
-import { Spinner, Input, DateTimePicker, Picklist, Option } from "react-rainbow-components";
+import { Spinner } from "react-rainbow-components";
 import request from "../axios";
 import RequestList from "../components/Home/RequestList/RequestList";
 
-const containerStyles = {
-  display: 'flex',
-  justifyContent: 'center',
-  padding: '30px 0'
-}
-const inputStyles = {
-  width: '150px',
-  marginRight: '10px'
-}
-
 const Home = () => {
-  const [bountyMin, setBountyMin] = useState<string>()
-  const [bountyMax, setBountyMax] = useState<string>()
-  const [xp, setXp] = useState<string>()
-  const [questGiver, setQuestGiver] = useState<string>()
-  const [name, setName] = useState<string>()
-  const [duration, setDuration] = useState<string>()
-
   const { isLoading, data: dataRequest } = useQuery("fetchRequest", () =>
-    request.get(
-      "/requests",
-      {
-        params: {
-          "bountyMin": bountyMin,
-          "bountyMax": bountyMax,
-          "awardedExperience": xp,
-          "questGiver": questGiver,
-          "name": name,
-          "duration": duration
-        },
-      }
-    )
+    request.get("/requests")
   );
 
   return (
     <>
       {isLoading && <Spinner />}
-      <div style={containerStyles}>
-        <Input
-            id="input-component-1"
-            placeholder="Nom"
-            style={inputStyles}
-            onChange={e => setName(e.target.value === '' ? undefined : e.target.value)}
-        />
-        <Input
-            type='number'
-            id="input-component-1"
-            placeholder="Prime min"
-            style={inputStyles}
-            onChange={e => setBountyMin(e.target.value === '' ? undefined : e.target.value)}
-        />
-        <Input
-            type='number'
-            id="input-component-1"
-            placeholder="Prime max"
-            style={inputStyles}
-            onChange={e => setBountyMax(e.target.value === '' ? undefined : e.target.value)}
-        />
-        <Input
-            type='number'
-            id="input-component-1"
-            placeholder="Exp"
-            style={inputStyles}
-            onChange={e => setXp(e.target.value === '' ? undefined : e.target.value)}
-        />
-        <Input
-            id="input-component-1"
-            placeholder="Commanditaire"
-            style={inputStyles}
-            onChange={e => setQuestGiver(e.target.value === '' ? undefined : e.target.value)}
-        />
-        <Input
-            type='number'
-            id="input-component-1"
-            placeholder="Durée"
-            style={inputStyles}
-            onChange={e => setDuration(e.target.value === '' ? undefined : e.target.value)}
-        />
-      </div>
       <RequestList requests={dataRequest?.data} />
     </>
   );
