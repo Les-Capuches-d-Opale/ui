32909fb5fa60608ae6c83b992b561c0b1dabc04a
killiancambert <killian.cambert@ynov.com>
2022-01-26 14:32:38 +0100

    wallet in the sidebar

---
+++ src/components/Sidebar/SidebarWallet.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import request from "../../axios";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Badge } from "react-rainbow-components";
+
+const SidebarWallet = () => {
+  const [wallet, setWallet] = useState(0);
+
+  const { data: admin } = useQuery("fetchAdmin", () =>
+    request.get("/administrators")
+  );
+
+  useEffect(() => {
+    if (admin?.data) {
+      setWallet(admin?.data.wallet);
+    }
+  }, admin?.data);
+
+  return (
+    <>
+      <Badge
+        title="Portefeuille de la guilde"
+        className="rainbow-m-around_medium "
+        variant="outline-brand"
+      >
+        <span>
+          {wallet} <FontAwesomeIcon icon={faCoins} />
+        </span>
+      </Badge>
+    </>
+  );
+};
+
+export default SidebarWallet;
